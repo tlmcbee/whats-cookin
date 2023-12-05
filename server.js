dfb0a61c73add6f8faa031b4608fa370d0b9ade8
tlmcbee <tlmcbeejr@gmail.com>
2023-12-05 15:15:52 -0800

    Adds Google OAuth

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override')
 
 require('dotenv').config()
 require('./config/database')
+require('./config/passport')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -24,6 +25,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
