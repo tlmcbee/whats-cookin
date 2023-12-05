bf95051289e8161b77fd55a2423aea33d69ba165
tlmcbee <tlmcbeejr@gmail.com>
2023-12-05 14:28:25 -0800

    Installs dependencies. Adds database

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport')
+const methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
