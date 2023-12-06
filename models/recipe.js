50e9aeb06fa217088cbde0212552dc8c8c0912fe
tlmcbee <tlmcbeejr@gmail.com>
2023-12-05 17:08:26 -0800

    Adds recipe model, recipeSchema, and reviewSchema

---
+++ models/recipe.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  title: {
+    type: String, 
+    required: true
+  },
+  cuisine: { 
+    type: String,
+    required: true
+  },
+  ingredients: { 
+    type: String,
+    required: true
+  },
+  method: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  addedBy: String,
+  userAvatar: String,
+  addedOn: Date,
+  reviews: [reviewSchema]
+})
+
+const reviewSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  made: {
+    type: String,
+    enum: ['YES' , 'NO']
+  },
+
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  userName: String,
+  userAvatar: String
+}, {
+  timestamps: true
+})
